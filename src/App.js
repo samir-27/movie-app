42cbc1421a981a4cbc785502260e31cc340e79cd
samir-27 <sumrasamir27@gmail.com>
2023-03-18 18:17:57 +0530

    save favourites

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/searchBox';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue]=useState('');
+	const [searchValue, setSearchValue] = useState('');
+	const [favourites, setFavourites] = useState([]);
 
 	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
@@ -16,9 +18,13 @@ const App = () => {
 		const responseJson = await response.json();
 
 		if (responseJson.Search) {
-   
-      setMovies(responseJson.Search);
+			setMovies(responseJson.Search);
 		}
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
 	};
 
 	useEffect(() => {
@@ -26,16 +32,43 @@ const App = () => {
 	}, [searchValue]);
 
 	return (
+		// <div className='container movie-app'>
+		// 	<div className='row d-flex align-items-center mt-4 mb-4'>
+		// 		<MovieListHeading heading='Movies' />
+		// 		<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+		// 	</div>
+		// 	<div className='row my-5'>
+		// 		<div className="block my-5">
+		// 			<MovieList
+		// 				movies={movies}
+		// 				handleFavouritesClick={addFavouriteMovie}
+		// 				favouriteComponent={AddFavourites}
+		// 			/>
+		// 		</div>
+
+		// 	</div>
+		// </div>
 		<div className='container-fluid movie-app'>
-      <div className="row  d-flex position-fixed align-items-center m-3">
-        <MovieListHeading className="position-fixed" heading="movies"/>
-        <SearchBox className="position-fixed" searchValue={searchValue} setSearchValue={setSearchValue}/>
-      </div>
-			<div className='row my-5'>
-        <div className="block my-5">
-        <MovieList movies={movies} />
-        </div>
-			
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<div className="block my-5">
+					<MovieList
+						movies={movies}
+						favouriteComponent={AddFavourites}
+						handleFavouritesClick={addFavouriteMovie}
+					/>
+				</div>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+			<div className="block my-5">
+				<MovieList movies={favourites} favouriteComponent={AddFavourites} />
+				</div>
 			</div>
 		</div>
 	);
---
+++ src/components/AddFavourites.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddFavourites = () =>{
+    return(
+        <>
+            <span>Add to favourites</span>
+        </>
+    )
+}
+export default AddFavourites
